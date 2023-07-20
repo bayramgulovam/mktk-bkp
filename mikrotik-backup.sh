b387ceae86d63f2899ce2077736142b8e87e6cd8
mamayonez <bayramgulov.am@yandex.ru>
2023-07-20 13:30:28 +0300

    added config.env

--- mikrotik-backup.sh
+++ mikrotik-backup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Импорт переменных из файла config.env
+source config.env
+
 # Функция для создания директории, если она не существует
 function create_directory_if_not_exists() {
     if [ ! -d "$1" ]; then
@@ -7,38 +10,29 @@ function create_directory_if_not_exists() {
     fi
 }
 
-# Диапазон IP-адресов для подключения
-IP_START="192.168.0.1"
-IP_END="192.168.0.10"
-
-# Параметры SFTP-сервера
-SFTP_HOST="192.168.2.1"
-SFTP_USER="your_username"
-SFTP_PASSWORD="your_password"
-SFTP_DIRECTORY="/path/to/backup/directory"
-
 # Перебор IP-адресов в диапазоне и выполнение действий
-for IP in $(seq -f '%.0f' $(echo $IP_START | tr '.' ' ') $(echo $IP_END | tr '.' ' ')); do
+for IP in $(seq -f '%.0f' $(echo $MikroTik_IP_START | tr '.' ' ') $(echo $MikroTik_IP_END | tr '.' ' ')); do
     # Получение имени устройства
-    IDENTITY=$(sshpass -p "your_password" ssh -o StrictHostKeyChecking=no -o PubkeyAuthentication=no admin@$IP '/system identity print value-name=name')
+    IDENTITY=$(sshpass -p "$MikroTik_PASSWORD" ssh -o StrictHostKeyChecking=no -o PubkeyAuthentication=no $MikroTik_USERNAME@$IP '/system identity print value-name=name')
 
     # Генерация имени файла резервной копии (Identity + дата)
     DATE=$(date +"%Y-%m-%d")
-    BACKUP_FILENAME="$IDENTITY-$DATE-backup.backup"
-    EXPORT_FILENAME="$IDENTITY-$DATE-export.rsc"
+    BACKUP_FILENAME="$BACKUP_DIRECTORY/$IDENTITY-$DATE-backup.backup"
+    EXPORT_FILENAME="$EXPORT_DIRECTORY/$IDENTITY-$DATE-export.rsc"
 
     # Создание резервной копии конфигурации
-    sshpass -p "your_password" ssh -o StrictHostKeyChecking=no -o PubkeyAuthentication=no admin@$IP "/system backup save name=$BACKUP_FILENAME"
+    sshpass -p "$MikroTik_PASSWORD" ssh -o StrictHostKeyChecking=no -o PubkeyAuthentication=no $MikroTik_USERNAME@$IP "/system backup save name=$BACKUP_FILENAME"
 
     # Экспорт конфигурации в файл
-    sshpass -p "your_password" ssh -o StrictHostKeyChecking=no -o PubkeyAuthentication=no admin@$IP "/export file=$EXPORT_FILENAME"
+    sshpass -p "$MikroTik_PASSWORD" ssh -o StrictHostKeyChecking=no -o PubkeyAuthentication=no $MikroTik_USERNAME@$IP "/export file=$EXPORT_FILENAME"
 
     # Копирование файлов на SFTP-сервер
-    create_directory_if_not_exists "$SFTP_DIRECTORY"
-    sshpass -p "$SFTP_PASSWORD" scp -o StrictHostKeyChecking=no "$BACKUP_FILENAME" "$SFTP_USER@$SFTP_HOST:$SFTP_DIRECTORY"
-    sshpass -p "$SFTP_PASSWORD" scp -o StrictHostKeyChecking=no "$EXPORT_FILENAME" "$SFTP_USER@$SFTP_HOST:$SFTP_DIRECTORY"
+    create_directory_if_not_exists "$EXPORT_DIRECTORY"
+    create_directory_if_not_exists "$BACKUP_DIRECTORY"
+    sshpass -p "$SFTP_PASSWORD" scp -o StrictHostKeyChecking=no "$BACKUP_FILENAME" "$SFTP_USER@$SFTP_HOST:$BACKUP_DIRECTORY"
+    sshpass -p "$SFTP_PASSWORD" scp -o StrictHostKeyChecking=no "$EXPORT_FILENAME" "$SFTP_USER@$SFTP_HOST:$EXPORT_DIRECTORY"
 
     # Удаление временных файлов резервной копии
-    sshpass -p "your_password" ssh -o StrictHostKeyChecking=no -o PubkeyAuthentication=no admin@$IP "/file remove $BACKUP_FILENAME"
-    sshpass -p "your_password" ssh -o StrictHostKeyChecking=no -o PubkeyAuthentication=no admin@$IP "/file remove $EXPORT_FILENAME"
+    sshpass -p "$MikroTik_PASSWORD" ssh -o StrictHostKeyChecking=no -o PubkeyAuthentication=no $MikroTik_USERNAME@$IP "/file remove $BACKUP_FILENAME"
+    sshpass -p "$MikroTik_PASSWORD" ssh -o StrictHostKeyChecking=no -o PubkeyAuthentication=no $MikroTik_USERNAME@$IP "/file remove $EXPORT_FILENAME"
 done
